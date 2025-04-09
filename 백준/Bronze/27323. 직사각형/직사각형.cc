33da0b741dee212d66c47c3c53126bda42a42883
wonjun <72507602+firepool7030@users.noreply.github.com>
2025-04-09 12:41:19 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+	int n, m;
+
+	cin >> n >> m;
+
+	cout << n * m << endl;
+
+	return 0;
+}
