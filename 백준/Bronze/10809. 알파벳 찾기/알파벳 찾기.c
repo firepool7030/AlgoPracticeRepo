0b82a6a76bdea8157cbe4db4c7b83c45d37c5e58
wonjun <72507602+firepool7030@users.noreply.github.com>
2025-04-10 16:34:11 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main(void) {
+	int cnt[26] = { 0, }, tmp;
+	char ar[101];
+	scanf("%s", ar);
+	int len = strlen(ar);
+	for (int i = 0; i < len; i++) {
+		tmp = ar[i] - 'a';
+		if (cnt[tmp] == 0)
+			cnt[tmp] = i + 1;
+	}
+	for (int i = 0; i < 26; i++) {
+		printf("%d ", cnt[i] - 1);
+	}
+	return 0;
+}
