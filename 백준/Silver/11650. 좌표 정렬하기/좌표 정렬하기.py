90aa0ffa8ed6dc0504e077877e85be3b7832246b
wonjun <72507602+firepool7030@users.noreply.github.com>
2025-04-10 18:16:07 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 272 ms, Memory: 47288 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -7,8 +7,7 @@ n = int(input())
 
 # 좌표를 tuple 형태로 저장 후 정렬
 points = [tuple(map(int, input().split())) for _ in range(n)]
-points = sorted(points)
 
 # 정렬된 좌표들을 오름차순으로 출력
-for x, y in points:
+for x, y in sorted(points):
     print(x, y)
