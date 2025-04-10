3e360d9b9d6b2882e0e09e7960967c3c1a2021f7
wonjun <72507602+firepool7030@users.noreply.github.com>
2025-04-10 18:23:29 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 224 ms, Memory: 54680 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -1,13 +1,14 @@
 # BOJ_11650_silver5
 import sys
 input = sys.stdin.readline
+output = sys.stdout.write
 
 # 입력 받을 횟수 n 입력
 n = int(input())
 
-# 좌표를 tuple 형태로 저장 후 정렬
+# 좌표를 tuple 형태로 저장
 points = [tuple(map(int, input().split())) for _ in range(n)]
 
-# 정렬된 좌표들을 오름차순으로 출력
-for x, y in sorted(points):
-    print(x, y)
+# x, y 튜플을 문자열로 바꾼 후 \n.join 함수로 이어서 붙이기 (print()로 해도 되지만, 여러번의 print()를 한 번으로 줄임)
+result = '\n'.join(f'{x} {y}' for x, y in sorted(points))
+output(result)
