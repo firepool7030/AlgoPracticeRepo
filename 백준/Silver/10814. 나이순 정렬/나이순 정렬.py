a39e9c94fd9f6b6cec8b19888000789e9bef4941
wonjun <72507602+firepool7030@users.noreply.github.com>
2025-04-11 15:41:23 +0900

    [Silver V] Title: 나이순 정렬, Time: 152 ms, Memory: 59868 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -7,6 +7,10 @@ n = int(input())
 # n개의 데이터 입력
 data = [input().split() for _ in range(n)]
 
-# 방법 1
-for age, name in sorted(data, key=lambda x:int(x[0])):
-    print(age, name)
+# # 방법 1
+# for age, name in sorted(data, key=lambda x:int(x[0])):
+#     print(age, name)
+
+#방법 2
+result = '\n'.join(f"{age} {name}" for age, name in sorted(data, key=lambda x:int(x[0])))
+print(result)
