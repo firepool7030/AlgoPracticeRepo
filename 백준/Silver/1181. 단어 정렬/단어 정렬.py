a5c8b76d6cfd299408e66e5789379b5195153200
wonjun <72507602+firepool7030@users.noreply.github.com>
2025-04-11 15:30:20 +0900

    [Silver V] Title: 단어 정렬, Time: 60 ms, Memory: 37024 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -3,11 +3,11 @@ import sys
 input = sys.stdin.readline
 output = sys.stdout.write
 
-# 반복 횟수 입력
+# 1. 반복 횟수 입력
 n = int(input())
-# n번 문자열 입력후 중복 제거
+# 2. n번 문자열 입력후 중복 제거
 words = [input().strip() for _ in range(n)]
+# 3. 중복 제거 후 알파벳 순서로 정렬
 words = sorted(set(words))
-
-# 1. 알파벳 순으로 정렬 후 2. 문자열 길이로 정렬 후 반환
+# 4. 다시 길이 순서로 정렬 후 반환
 print(*sorted(words, key=len), sep='\n')
