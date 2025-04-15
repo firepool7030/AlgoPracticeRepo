28ae4d4bfc2324a2551c80c4616176f6d4a66305
wonjun <72507602+firepool7030@users.noreply.github.com>
2025-04-15 16:29:35 +0900

    [Silver V] Title: 숫자 카드, Time: 372 ms, Memory: 121088 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -8,3 +8,10 @@ m = int(input())
 arr2 = list(map(int, input().split()))
 
 print(' '.join('1' if num in arr1 else '0' for num in arr2))
+
+"""
+1. 배열의 존재여부 확인 -> set 사용
+    - set은 hash table을 사용해서 o(1)의 탐색 시간을 가짐
+2. 정렬된 배열에서의 탐색 -> bisect_left 사용
+    - 이진탐색 기반으로 o(log n)의 탐색시간을 가짐
+"""
