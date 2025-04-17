90d81d64b6aed3ce16896427aa278562b5a86cf0
wonjun <72507602+firepool7030@users.noreply.github.com>
2025-04-17 16:16:14 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 136 ms, Memory: 43724 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,16 @@
+# BOJ_7785_silver5
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = set()
+
+for _ in range(n):
+    name, status = input().strip().split()
+    if status == 'enter':
+        arr.add(name)
+    elif status == 'leave':
+        arr.discard(name)
+
+result = '\n'.join(f'{name}' for name in sorted(arr, reverse=True))
+print(result)
