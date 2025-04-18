4401083870e0b3164dd52c2f168433ddd84d7b37
wonjun <72507602+firepool7030@users.noreply.github.com>
2025-04-18 20:49:09 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main(void){
+
+    char ar[101];
+    scanf("%s", ar);
+    printf("%d", strlen(ar));
+
+    return 0;
+}
