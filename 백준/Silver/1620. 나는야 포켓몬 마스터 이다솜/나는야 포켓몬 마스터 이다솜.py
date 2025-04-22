95d745bf59b5e58e7cebc60b302ec83e3b0a9e80
wonjun <72507602+firepool7030@users.noreply.github.com>
2025-04-22 23:39:08 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 196 ms, Memory: 51528 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,20 +1,21 @@
 # BOJ_1620_silver4
 import sys
 input = sys.stdin.readline
+output = sys.stdout.write
 
 n, m = map(int, input().split())
 pokemon_name = {}
 pokemon_idx = {}
 
-for i in range(n):
+for i in range(1, n + 1):
     name = input().strip()
-    pokemon_idx[i + 1] = name
-    pokemon_name[name] = i + 1
+    pokemon_idx[i] = name
+    pokemon_name[name] = i
 
 for _ in range(m):
     tmp = input().strip()
     if tmp.isdigit():
         num = int(tmp)
-        print(pokemon_idx[num])
+        output(pokemon_idx[num] + "\n")
     else:
-        print(pokemon_name[tmp])
+        output(str(pokemon_name[tmp]) + "\n")
