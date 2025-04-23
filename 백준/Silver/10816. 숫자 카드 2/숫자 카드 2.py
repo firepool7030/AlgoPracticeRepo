e5ee7932ca14da956361948cf872b4c67d87aee0
wonjun <72507602+firepool7030@users.noreply.github.com>
2025-04-23 23:26:20 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 588 ms, Memory: 127928 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -0,0 +1,19 @@
+#BOJ_10816_silver4
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+lst = list(map(int, input().strip().split()))
+arr = {}
+
+for tmp in lst:
+    if tmp in arr:
+        arr[tmp] += 1
+    else:
+        arr.update({tmp: 1})
+
+m = int(input())
+lst = list(map(int, input().strip().split()))
+
+result = ' '.join(str(arr.get(tmp, 0)) for tmp in lst)
+print(result)
