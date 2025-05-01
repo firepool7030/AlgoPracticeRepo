705a8f4b2e0e27cb29f21aafdd58084f87f6599b
wonjun <72507602+firepool7030@users.noreply.github.com>
2025-05-01 22:27:37 +0900

    [Silver IV] Title: 가로수, Time: 100 ms, Memory: 41928 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.py
+++ 백준/Silver/2485. 가로수/가로수.py
@@ -1,24 +1,25 @@
 #BOJ_2485_silver4
-#가독성 향상 version
 import sys
 input = sys.stdin.readline
 
+# 최대공약수 구하는 함수
 def gcd (x:int, y:int):
     while y:
         x, y = y, x % y
     return x
 
+# 입력 받기
 n = int(input())
 arr1 = [int(input()) for _ in range(n)]
+
+# 인접한 가로수 사이의 거리 계산 후 저장
 arr2 = [arr1[i] - arr1[i - 1] for i in range(1, n)]
 
+# 거리간 최대공약수 구하기
 total_gcd = arr2[0]
-total_cnt = 0
-
-for i in arr2:
+for i in arr2[1:]:
     total_gcd = gcd(total_gcd, i)
 
-for i in arr2:
-    total_cnt += i // total_gcd - 1
-
-print(total_cnt)
+# 최대공약수 처리 후 합 반환
+additional_trees = sum((distance // total_gcd - 1) for distance in arr2)
+print(additional_trees)
