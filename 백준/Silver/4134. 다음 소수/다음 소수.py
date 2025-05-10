73979c7c89e229b96b257b604363b078b8110c6f
wonjun <72507602+firepool7030@users.noreply.github.com>
2025-05-10 22:02:57 +0900

    [Silver IV] Title: 다음 소수, Time: 892 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4134. 다음 소수/다음 소수.py
+++ 백준/Silver/4134. 다음 소수/다음 소수.py
@@ -5,7 +5,11 @@ input = sys.stdin.readline
 def is_prime(x):
     if x < 2:
         return False
-    for i in range(2, int(x ** 0.5) + 1):
+    if x == 2:
+        return True
+    if x % 2 == 0:
+        return False
+    for i in range(3, int(x ** 0.5) + 1, 2):
         if x % i == 0:
             return False
     return True
