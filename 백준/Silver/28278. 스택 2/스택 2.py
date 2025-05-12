a368399e9faf7c06efab60b0c708c6abf153335c
wonjun <72507602+firepool7030@users.noreply.github.com>
2025-05-12 14:51:16 +0900

    [Silver IV] Title: 스택 2, Time: 1152 ms, Memory: 72404 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.py
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -6,30 +6,22 @@ stack = []
 x = int(input())
 
 for i in range(x):
-    nums = list(map(int, input().split()))
-    n = nums[0]
+    n, *nums = map(int, input().split())
+    # 정수 x 추가
     if n == 1:
-        m = nums[1]
-        stack.append(m)
-        continue
+        stack.append(nums[0])
+    # 맨 위 정수 pop 후 출력 / -1
     elif n == 2:
         if stack:
             print(stack.pop())
         else:
             print(-1)
-        continue
+    # 스택 정수 개수 출력
     elif n == 3:
         print(len(stack))
-        continue
+    # 스택 empy = 1 / 0
     elif n == 4:
-        if len(stack) == 0:
-            print(1)
-        else:
-            print(0)
-        continue
+        print(0 if stack else 1)
+    # 맨위 정수 출력 / -1
     elif n == 5:
-        if stack:
-            print(stack[-1])
-        else:
-            print(-1)
-        continue
+        print(stack[-1] if stack else -1)
