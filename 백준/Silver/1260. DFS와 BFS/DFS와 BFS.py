b0ec090ce336fb08bbe4d5fa223e6795668f9cb5
wonjun <72507602+firepool7030@users.noreply.github.com>
2025-05-28 14:40:30 +0900

    [Silver II] Title: DFS와 BFS, Time: 64 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -3,50 +3,49 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-# 정점, 간선, 시작점 입력
+# 정점, 간선, 시작점 입력, 그래프 선언
 n, m, v = map(int, input().split())
-
-# 그래프 초기화 (0번 인덱스 사용 x)
 graph = [[] for _ in range(n + 1)]
 
-# 양방향 간선 정보 입력
-for i in range(m):
+# 간선 입력 후 오름차순 정렬
+for _ in range(m):
     start, end = map(int, input().split())
     graph[start].append(end)
     graph[end].append(start)
 
-# 각 정점의 간선 오름차순으로 정렬
-for adx in graph:
-    adx.sort()
+for lst in graph:
+    lst.sort()
+
+# dfs, bfs 함수에 사용되는 방문 리스트 선언
+dfs_visited = [False] * (n + 1)
+bfs_visited = [False] * (n + 1)
 
 # dfs 구현
-def dfs(v:int , visited: list) -> None:
-    visited[v] = True
-    print(v, end=' ')
-    for node in graph[v]:
-        if visited[node] is False:
-            dfs(node, visited)
+def dfs(vertex: int) -> None:
+    print(vertex, end=' ')
+    dfs_visited[vertex] = True
+    for node in graph[vertex]: # 정점의 노드들을 순서대로 깊이우선 탐색 진행
+        if dfs_visited[node] is False:
+            dfs(node)
 
-def bfs(v: int) -> None:
-    visited = [False] * (n + 1)
+# bfs 구현
+def bfs(vertex: int) -> None:
     queue = deque()
-    print(v, end=' ')
-    visited[v] = True
-    for node in graph[v]:
+    print(vertex, end=' ')
+    bfs_visited[vertex] = True
+    for node in graph[vertex]:
         queue.appendleft(node)
+    # 데크를 사용한 bfs 구현
     while queue:
         tmp = queue.pop()
-        if visited[tmp] is True:
+        if bfs_visited[tmp] is True:
             continue
-        visited[tmp] = True
         print(tmp, end=' ')
+        bfs_visited[tmp] = True
         for node in graph[tmp]:
-            if visited[node] is False:
+            if bfs_visited[node] is False:
                 queue.appendleft(node)
 
-
-# dfs에서 사용되는 방문 리스트 생성
-visited = [False] * (n + 1)
-dfs(v, visited)
+dfs(v)
 print()
 bfs(v)
