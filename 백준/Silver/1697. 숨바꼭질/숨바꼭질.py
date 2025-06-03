17708af308b9d1a8b5963491f222af8a4edce1b6
wonjun <72507602+firepool7030@users.noreply.github.com>
2025-06-03 22:42:52 +0900

    [Silver I] Title: 숨바꼭질, Time: 96 ms, Memory: 36356 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,3 +1,4 @@
+#BOJ_1697_silver1
 import sys
 from collections import deque
 input = sys.stdin.readline
@@ -8,16 +9,15 @@ def bfs(start:int, end:int) -> None:
     MAX = 100001
     visited = [False] * MAX
     queue = deque()
-    queue.append((start, 0))
+    queue.append((start, 0)) # bfs queue안에 시작점과 최초 시간 삽입
     visited[start] = True
-
     while queue:
         cur, time = queue.popleft()
-        if cur == end:
+        if cur == end: # 목적지에 도착하면 탐색 시간 반환
             print(time)
             return
         for pos in (cur - 1, cur + 1, cur * 2):
-            if 0 <= pos < MAX and not visited[pos]:
+            if 0 <= pos < MAX and visited[pos] == False:
                 visited[pos] = True
                 queue.append((pos, time + 1))
 
