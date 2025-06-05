29ae1171f69797e3951031b6ae35883d72bc184b
wonjun <72507602+firepool7030@users.noreply.github.com>
2025-06-05 23:07:22 +0900

    [Silver IV] Title: 로프, Time: 84 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.py
+++ 백준/Silver/2217. 로프/로프.py
@@ -3,14 +3,7 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-max_weight = list(int(input()) for _ in range(n))
-max_weight.sort(reverse = True)
-max_ = 0
+ropes = list(int(input()) for _ in range(n))
+ropes.sort(reverse = True)
 
-for i in range(n):
-    tmp = (i + 1) * max_weight[i]
-    #print(tmp)
-    if max_ < tmp:
-        max_ = tmp
-
-print(max_)
+print(max(ropes[i] * (i + 1) for i in range(n)))
